1a474a13cc24f01bbe4e6ee9e4c9c0c1cd878ee2
Jackson Duke <JacksonBDuke@gmail.com>
2018-10-01 16:53:31 -0500

    Changed implementation of injected variable to annotation-driven.

--- src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -4,14 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.pluralsight.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    @Value("${dbUsername}")
 	private String dbUsername;
-
-	public void setDbUsername(String dbUsername) {
-		this.dbUsername = dbUsername;
-	}
 
 	@Override
 	public List<Customer> findAll() {
